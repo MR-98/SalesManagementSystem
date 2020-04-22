2e43fadfc6799a099d9065cad3a9769baf87cce5
matlukomski94 <matlukomski94@gmail.com>
2020-04-22 19:33:18 +0200

    implemented adding Category entity

--- src/main/java/com/zpi/salesmanagementsystem/controllers/CategoryController.java
+++ src/main/java/com/zpi/salesmanagementsystem/controllers/CategoryController.java
@@ -1,14 +1,35 @@
 package com.zpi.salesmanagementsystem.controllers;
 
 
+import com.zpi.salesmanagementsystem.models.Category;
+import com.zpi.salesmanagementsystem.repositories.CategoryRepository;
+import com.zpi.salesmanagementsystem.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CategoryController {
 
-    public void getAll(){
+    private final CategoryService categoryService;
 
+    @Autowired
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
     }
 
+    @GetMapping("/categories")
+    public List<Category> getAllCategory(){
+        return this.categoryService.getAllCategory();
+    }
+
+    @PostMapping("/categories")
+    void addCategory(@RequestBody Category category){
+        this.categoryService.addCategory(category);
+    }
 
 }
--- src/main/java/com/zpi/salesmanagementsystem/services/CategoryService.java
+++ src/main/java/com/zpi/salesmanagementsystem/services/CategoryService.java
@@ -1,4 +1,27 @@
 package com.zpi.salesmanagementsystem.services;
 
+import com.zpi.salesmanagementsystem.models.Category;
+import com.zpi.salesmanagementsystem.repositories.CategoryRepository;
+import org.apache.commons.collections4.IterableUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository){
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategory(){
+        return IterableUtils.toList(this.categoryRepository.findAll());
+    }
+
+    public void addCategory(Category category){
+        this.categoryRepository.save(category);
+    }
 }
