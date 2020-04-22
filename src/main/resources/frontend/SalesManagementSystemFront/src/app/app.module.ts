9edfdfd9fdc3c8d14d2a31f234d554773c29830d
rosiak.mateusz <215866@edu.p.lodz.pl>
2020-04-22 15:58:18 +0200

    Install bootstrap, add logo and navbar

--- src/main/resources/frontend/SalesManagementSystemFront/src/app/app.module.ts
+++ src/main/resources/frontend/SalesManagementSystemFront/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from "./components/header/header.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
