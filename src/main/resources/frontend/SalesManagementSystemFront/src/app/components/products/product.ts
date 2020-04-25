8d470753fb00a568f3d1da90eb1bed928c69eadd
matlukomski94 <matlukomski94@gmail.com>
2020-04-25 19:36:10 +0200

    addition of product table display

---
+++ src/main/resources/frontend/SalesManagementSystemFront/src/app/components/products/product.ts
@@ -0,0 +1,7 @@
+export class Product {
+  id: number;
+  name: string;
+  basePrice: number;
+  preferredSalePrice: number;
+  category: string;
+}
---
+++ src/main/resources/frontend/SalesManagementSystemFront/src/app/services/product.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Product} from "../components/products/product";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  url: string = 'http://localhost:8080/products';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.url);
+  }
+}
